122143d57f01b57cb8bccffb9558002f3c3161bc
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-20 14:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
 
-class Square extends Animal {}
+    // Getter for width
+    get width() {
+        return this._width;
+    }
+
+    // Getter for height
+    get height() {
+        return this._height;
+    }
+
+    // Method to calculate and return the area of the rectangle
+    getArea() {
+        return this._width * this._height;
+    }
+}
+class Square extends Rectangle {
+	constructor(side){
+		super(side, side);
+	}
+
+	 getPerimeter() {
+        return 4 * this.width; // Perimeter of square: 4 * side
+    }
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
